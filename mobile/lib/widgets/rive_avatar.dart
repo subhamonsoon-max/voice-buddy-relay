b5151c6f13596211162e3d941bdd0a69341bcd8a
subhammaharana-prog <subham.maharana@monsoontech.ai>
2026-09-12 00:53:26 +0530

    Fix LinearGradient ambiguous import in rive_avatar and update widget test

--- mobile/lib/widgets/rive_avatar.dart
+++ mobile/lib/widgets/rive_avatar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
+import 'package:rive/rive.dart' hide LinearGradient;
 import '../services/websocket_service.dart';
 
 class RiveAvatarWidget extends StatefulWidget {
